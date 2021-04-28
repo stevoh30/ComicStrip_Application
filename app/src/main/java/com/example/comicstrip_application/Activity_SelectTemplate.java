d14f2e12e149f17c87e63319f7e797edebc2a87f
stevoh30 <36320944+stevoh30@users.noreply.github.com>
2021-04-27 20:22:47 -0700

    Added Template Selection Activity
    added template selection activity page

---
+++ app/src/main/java/com/example/comicstrip_application/Activity_SelectTemplate.java
@@ -0,0 +1,14 @@
+package com.example.comicstrip_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_SelectTemplate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__select_template);
+    }
+}
