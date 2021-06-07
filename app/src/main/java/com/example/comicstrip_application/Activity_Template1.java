aef07970439d66f41782592cb8ebdb4f99f2616c
stevoh30 <36320944+stevoh30@users.noreply.github.com>
2021-06-07 16:32:38 -0700

    Added Fragment for image layout
    Created a fragment for image layout, replacing static image layout on activitiy_1. This will allow us to switch layouts while keeping the button structure intact.

--- app/src/main/java/com/example/comicstrip_application/Activity_Template1.java
+++ app/src/main/java/com/example/comicstrip_application/Activity_Template1.java
@@ -8,6 +8,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.Manifest;
 import android.app.Activity;
@@ -48,10 +50,9 @@ import java.util.Date;
 public class Activity_Template1 extends AppCompatActivity {
 
     //Declare variables
-    private ImageView imageView1, imageView2, imageView3,imageViewCreateText, imageViewCreateBubble2, imageViewCreateGraphics, imageViewChangeBackground;
+    private ImageView imageViewCreateText, imageViewCreateBubble2, imageViewCreateGraphics, imageViewChangeBackground;
     private Button btnFlip, btnDeleteTxt, btnMaximize, btnMinimize, btnScreenshot;
     private Dialog dialog;
-    byte imageViewSelector = 0;
     private Context context;
     private String m_Text = "";
     private Boolean delete = false;
@@ -59,8 +60,6 @@ public class Activity_Template1 extends AppCompatActivity {
     private Boolean maximize = false;
     private Boolean minimize = false;
     ConstraintLayout layout;
-
-    Float scalingFactor = 0.75f; // scale down variable
 
 
     // Request code gallery
@@ -80,9 +79,6 @@ public class Activity_Template1 extends AppCompatActivity {
         setContentView(R.layout.activity__template1);
 
         //Initialize variables
-        imageView1 = findViewById(R.id.imageViewPhoto1);
-        imageView2 = findViewById(R.id.imageViewPhoto2);
-        imageView3 = findViewById(R.id.imageViewPhoto3);
         imageViewCreateText = findViewById(R.id.imgCreateText);
         imageViewCreateBubble2 = findViewById(R.id.imgCreateBubble2);
         imageViewCreateGraphics = findViewById(R.id.imgCreateGraphics);
@@ -95,6 +91,22 @@ public class Activity_Template1 extends AppCompatActivity {
         layout = findViewById(R.id.myLayout);
         context = this;
         dialog = new Dialog(this);
+
+        //display fragment_one at the start of this activity
+        if(savedInstanceState == null){
+            //create a fragment manager
+            FragmentManager fmgr = this.getSupportFragmentManager();
+            //create a fragment transaction
+            FragmentTransaction ft = fmgr.beginTransaction();
+            //create the fragment object
+            Fragment_ImageLayout1 f1 = new Fragment_ImageLayout1();
+            //add fragment to this activity
+            //the FragmentTransaction defines methods: add, replace,remove
+            ft.add(R.id.frame1,f1,"FRAGMENT_ONE");
+
+            //commit
+            ft.commit();
+        }
 
 
 
@@ -251,31 +263,6 @@ public class Activity_Template1 extends AppCompatActivity {
             }
         });
 
-        // create onClick functionality for imageviews to operate as buttons
-        imageView1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageViewSelector = 1;
-                //getImageFromGalleryPermissions();
-                showImageOptionDialog();
-            }
-        });
-        imageView2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageViewSelector = 2;
-                //getImageFromGalleryPermissions();
-                showImageOptionDialog();
-            }
-        });
-        imageView3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageViewSelector = 3;
-                //getImageFromGalleryPermissions();
-                showImageOptionDialog();
-            }
-        });
     }
     // method that calls a dialog to give the users options for which chatboxes they would
     // like to generate
@@ -488,174 +475,6 @@ public class Activity_Template1 extends AppCompatActivity {
         return flipped_bitmap;
     }
 
-    // create image dialog box, allowing user to choose between gallery and taking photo
-    private void showImageOptionDialog(){
-        final String[] options = getResources().getStringArray(R.array.image_options);
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle(R.string.dialog_image_options)
-                .setItems(options, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                        switch (which){
-                            case 0:
-                                if(ContextCompat.checkSelfPermission(Activity_Template1.this, Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED)
-                                {
-
-                                    getImageFromGallery();
-
-                                }else{
-                                    if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
-                                        /**
-                                         return false if user asked click on "Don't ask me again" box
-                                         or the permission is dissable on the device
-                                         your dialog with user won't be displayed
-                                         */
-                                        /**
-                                         * returns true, if the user rejected before but is trying to access again
-                                         * then your dialog to convince the user why they should grant it, will show
-                                         */
-                                        Toast.makeText(Activity_Template1.this,"grant it this permission will allow you to access storage from this app", Toast.LENGTH_LONG).show();
-                                    }
-                                    /**
-                                     * permission is not already granted. request it
-                                     */
-                                    //request it
-                                    ActivityCompat.requestPermissions(Activity_Template1.this,
-                                            new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
-                                            REQUEST_STORAGE_CODE);
-                                }
-
-                                break;
-                            case 1:
-                                if(ContextCompat.checkSelfPermission(Activity_Template1.this, Manifest.permission.WRITE_EXTERNAL_STORAGE )== PackageManager.PERMISSION_GRANTED)
-                                {
-
-                                    capturePictureFromCamera();
-
-                                }else{
-                                    if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
-                                        /**
-                                         return false if user asked click on "Don't ask me again" box
-                                         or the permission is dissable on the device
-                                         your dialog with user won't be displayed
-                                         */
-                                        /**
-                                         * returns true, if the user rejected before but is trying to access again
-                                         * then your dialog to convince the user why they should grant it, will show
-                                         */
-                                        Toast.makeText(Activity_Template1.this,"grant it this permission will allow you to make some change from this app",
-                                                Toast.LENGTH_LONG).show();
-                                    }
-
-                                    /**
-                                     * permission is not already granted. request it
-                                     */
-
-                                    //request it
-                                    ActivityCompat.requestPermissions(Activity_Template1.this,
-                                            new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE },
-                                            REQUEST_STORAGE2_CODE);
-                                }
-                                break;
-                        }
-                    }
-                });
-
-        AlertDialog dialog = builder.create();
-        dialog.show();
-    }
-
-    // Permissions
-    //callback from the requestPermissions dialog ===============================================
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        //check grantResults to find out if user clicked on grant or deny
-        if(requestCode ==REQUEST_STORAGE_CODE){
-            //check grantResults to find out if user clicked on grant or deny
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getImageFromGallery();
-            }else{
-                Toast.makeText(this,"permission denied. won't be able to use the phone app",Toast.LENGTH_LONG).show();
-            }
-        }
-        if(requestCode ==REQUEST_STORAGE2_CODE){
-            //check grantResults to find out if user clicked on grant or deny
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getImageFromGallery();
-            }else{
-                Toast.makeText(this,"permission denied. won't be able to use the phone app",Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    //Open phone gallery ==================================================================
-    private void getImageFromGalleryPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            getImageFromGallery();
-        } else {
-            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                Toast.makeText(this, "grant it this permission will allow you to choose pictures",
-                        Toast.LENGTH_LONG).show();
-            }
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    GALLERY_REQUEST);
-        }
-    }
-
-    private void getImageFromGallery() {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent, GALLERY_REQUEST);
-    }
-    private void capturePictureFromCamera(){
-        Intent cameraIntent = new Intent();
-        cameraIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-    }
-    // Activity Result
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        //Check if the intent was to pick image, was successful and an image was picked
-        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK && data != null) {
-            //Get selected image uri here
-            Uri imageUri = data.getData();
-            // uses imageViewSelector variable to determine what imageview to populate
-            switch(imageViewSelector) {
-                case 1:
-                    imageView1.setImageURI(imageUri);
-                    break;
-                case 2:
-                    imageView2.setImageURI(imageUri);
-                    break;
-                case 3:
-                    imageView3.setImageURI(imageUri);
-                    break;
-            }
-        }
-        else if(requestCode == CAMERA_REQUEST && resultCode == RESULT_OK){
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-            switch(imageViewSelector) {
-                case 1:
-                    //Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-                    imageView1.setImageBitmap(bitmap);
-                    break;
-                case 2:
-                    //Bitmap bitmap2 = (Bitmap) data.getExtras().get("data");
-                    imageView2.setImageBitmap(bitmap);
-                    break;
-                case 3:
-                   // Bitmap bitmap3 = (Bitmap) data.getExtras().get("data");
-                    imageView3.setImageBitmap(bitmap);
-                    break;
-            }
-        }
-    }
 
     //screenshot part
     private void takeScreenShot(View view) {
---
+++ app/src/main/java/com/example/comicstrip_application/Fragment_ImageLayout1.java
@@ -0,0 +1,247 @@
+package com.example.comicstrip_application;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+///**
+// * A simple {@link Fragment} subclass.
+// * Use the {@link Fragment_ImageLayout1#newInstance} factory method to
+// * create an instance of this fragment.
+// */
+public class Fragment_ImageLayout1 extends Fragment {
+
+    // Request code gallery
+    public static final int REQUEST_STORAGE_CODE = 1;
+    public static final int REQUEST_STORAGE2_CODE = 3;
+    private static final int GALLERY_REQUEST = 9;
+    private static final int CAMERA_REQUEST = 11;
+    byte imageViewSelector = 0;
+    private ImageView imageView_Photo1, imageView_Photo2, imageView_Photo3;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view =  inflater.inflate(R.layout.fragment__image_layout1, container,false);
+        view.setBackgroundColor(Color.BLACK);
+        imageView_Photo1 = view.findViewById(R.id.imgPhoto1);
+        imageView_Photo2 = view.findViewById(R.id.imgPhoto2);
+        imageView_Photo3 = view.findViewById(R.id.imgPhoto3);
+
+        // create onClick functionality for imageviews to operate as buttons
+        imageView_Photo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageViewSelector = 1;
+                //getImageFromGalleryPermissions();
+                showImageOptionDialog();
+            }
+        });
+        imageView_Photo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageViewSelector = 2;
+                //getImageFromGalleryPermissions();
+                showImageOptionDialog();
+            }
+        });
+        imageView_Photo3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageViewSelector = 3;
+                //getImageFromGalleryPermissions();
+                showImageOptionDialog();
+            }
+        });
+
+        return view;
+
+    }
+
+    // create image dialog box, allowing user to choose between gallery and taking photo
+    private void showImageOptionDialog(){
+        final String[] options = getResources().getStringArray(R.array.image_options);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(R.string.dialog_image_options)
+                .setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        switch (which){
+                            case 0:
+                                if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED)
+                                {
+
+                                    getImageFromGallery();
+
+                                }else{
+                                    if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
+                                        /**
+                                         return false if user asked click on "Don't ask me again" box
+                                         or the permission is dissable on the device
+                                         your dialog with user won't be displayed
+                                         */
+                                        /**
+                                         * returns true, if the user rejected before but is trying to access again
+                                         * then your dialog to convince the user why they should grant it, will show
+                                         */
+                                        Toast.makeText(getActivity(),"grant it this permission will allow you to access storage from this app", Toast.LENGTH_LONG).show();
+                                    }
+                                    /**
+                                     * permission is not already granted. request it
+                                     */
+                                    //request it
+                                    ActivityCompat.requestPermissions(getActivity(),
+                                            new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                                            REQUEST_STORAGE_CODE);
+                                }
+
+                                break;
+                            case 1:
+                                if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE )== PackageManager.PERMISSION_GRANTED)
+                                {
+
+                                    capturePictureFromCamera();
+
+                                }else{
+                                    if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                                        /**
+                                         return false if user asked click on "Don't ask me again" box
+                                         or the permission is dissable on the device
+                                         your dialog with user won't be displayed
+                                         */
+                                        /**
+                                         * returns true, if the user rejected before but is trying to access again
+                                         * then your dialog to convince the user why they should grant it, will show
+                                         */
+                                        Toast.makeText(getContext(),"grant it this permission will allow you to make some change from this app",
+                                                Toast.LENGTH_LONG).show();
+                                    }
+
+                                    /**
+                                     * permission is not already granted. request it
+                                     */
+
+                                    //request it
+                                    ActivityCompat.requestPermissions(getActivity(),
+                                            new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE },
+                                            REQUEST_STORAGE2_CODE);
+                                }
+                                break;
+                        }
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    // Permissions
+    //callback from the requestPermissions dialog ===============================================
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //check grantResults to find out if user clicked on grant or deny
+        if(requestCode ==REQUEST_STORAGE_CODE){
+            //check grantResults to find out if user clicked on grant or deny
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getImageFromGallery();
+            }
+        }
+        if(requestCode ==REQUEST_STORAGE2_CODE){
+            //check grantResults to find out if user clicked on grant or deny
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getImageFromGallery();
+            }
+        }
+    }
+
+    //Open phone gallery ==================================================================
+    private void getImageFromGalleryPermissions() {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            getImageFromGallery();
+        } else {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+//                Toast.makeText(this, "grant it this permission will allow you to choose pictures", Toast.LENGTH_LONG).show();
+            }
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    GALLERY_REQUEST);
+        }
+    }
+
+    private void getImageFromGallery() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_REQUEST);
+    }
+    private void capturePictureFromCamera(){
+        Intent cameraIntent = new Intent();
+        cameraIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+    // Activity Result
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Check if the intent was to pick image, was successful and an image was picked
+        if (requestCode == GALLERY_REQUEST && resultCode == getActivity().RESULT_OK && data != null) {
+            //Get selected image uri here
+            Uri imageUri = data.getData();
+            // uses imageViewSelector variable to determine what imageview to populate
+            switch(imageViewSelector) {
+                case 1:
+                    imageView_Photo1.setImageURI(imageUri);
+                    break;
+                case 2:
+                    imageView_Photo2.setImageURI(imageUri);
+                    break;
+                case 3:
+                    imageView_Photo3.setImageURI(imageUri);
+                    break;
+            }
+        }
+        else if(requestCode == CAMERA_REQUEST && resultCode == getActivity().RESULT_OK){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            switch(imageViewSelector) {
+                case 1:
+                    //Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    imageView_Photo1.setImageBitmap(bitmap);
+                    break;
+                case 2:
+                    //Bitmap bitmap2 = (Bitmap) data.getExtras().get("data");
+                    imageView_Photo2.setImageBitmap(bitmap);
+                    break;
+                case 3:
+                    // Bitmap bitmap3 = (Bitmap) data.getExtras().get("data");
+                    imageView_Photo3.setImageBitmap(bitmap);
+                    break;
+            }
+        }
+    }
+
+}
